ed831607c0fb42dffe09d0115c7c1d3353c5c93e
Bhashini123868 <bhashimunasinghe286@gmail.com>
2025-04-29 12:57:05 +0530

    4th commit

--- src/main/java/edu/icet/entity/EmployeeEntity.java
+++ src/main/java/edu/icet/entity/EmployeeEntity.java
@@ -1,10 +1,16 @@
 package edu.icet.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 @Data
@@ -18,17 +24,27 @@ public class EmployeeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @NotBlank(message = "Name is required")
+    @Pattern(regexp = "^[a-zA-Z ]*$", message = "Name must contain only alphabetic characters and spaces")
+    @Size(min = 2, max = 100, message = "Name must be less than 100 characters")
+    @Column(nullable = false)
     private String name;
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     @Column(nullable = false, unique = true)
     private String email;
 
+    @NotBlank(message = "Department is required")
+    @Pattern(regexp = "^(HR|IT|Finance|Operations)$", message = "Department must be HR,IT< Finance, or Operations")
     @Column(nullable = false)
     private String department;
 
-    @Column(updatable = false)
+    @CreationTimestamp
+    @Column(name = "create_at", nullable = false, updatable = false)
     private LocalDateTime createAt;
 
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updateAt;
 }
