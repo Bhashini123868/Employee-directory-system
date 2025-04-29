248f306f136ccac513c312e87663b5fd3aef81d3
Bhashini123868 <bhashimunasinghe286@gmail.com>
2025-04-29 11:37:11 +0530

    third commit

--- src/main/java/edu/icet/controller/EmployeeController.java
+++ src/main/java/edu/icet/controller/EmployeeController.java
@@ -4,23 +4,37 @@ import edu.icet.dto.Employee;
 import edu.icet.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @CrossOrigin
 @RestController
-@RequestMapping("employee")
+@RequestMapping("/employee")
 @RequiredArgsConstructor
 public class EmployeeController {
     private final EmployeeService service;
 
+    @GetMapping("/get-all")
     public ResponseEntity<List<Employee>> getAllEmployees(){
         return ResponseEntity.of(Optional.ofNullable(service.getAllEmployees()));
     }
 
+    @PostMapping("/create")
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee emmployee){
+        return ResponseEntity.ok(service.createEmployee(emmployee));
+    }
+
+    @GetMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody  Employee employee){
+        return ResponseEntity.ok(service.updateEmployee(id, employee));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id){
+        service.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
